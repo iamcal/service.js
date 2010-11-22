762e4d84d507d49a85a113175a022d47e9ffa3cd
Cal Henderson <cal@iamcal.com>
2010-11-21 18:10:03 -0600

    first checkin

---
+++ README.md
@@ -0,0 +1,41 @@
+This module allows you to turn your node app into a linux init.d daemon with a minimum of hassle.
+It requires <a href="https://github.com/indexzero/daemon.node">daemon.node</a>.
+
+First, write a wrapper/preamble:
+
+	#!/usr/local/bin/node
+
+	require('service').run({
+		lockFile: '/var/run/my_app.pid',
+		logFile : '/var/log/my_app.log',
+	});
+
+	// your app goes here...
+
+The execute it:
+
+	./wrapper.js [start|stop|restart|status|simple]
+
+The first commands do what you would expect, running the app as a daemon.
+The <code>simple</code> command allows you to run the app in interactive mode for testing.
+
+In daemon mode, output sent furing startup will be echo'd to the console, while anything sent 
+after the first timer/event will go to the log file.
+
+Unfortunately <code>chkconfig</code> requires extra comment lines at the top of the
+file that aren't node compatible, so you may want to call your js wrapper from a shell
+script like this:
+
+	#!/bin/sh
+	# chkconfig: 2345 95 20
+	# description: my_app
+	# processname: node
+	/usr/local/my_app/wrapper.js "$@"
+
+Then to get everything going:
+
+	ln -s /usr/local/my_app/init.sh /etc/init.d/my_app
+	chkconfig --add my_app
+	chkconfig my_app on
+	/etc/init.d/my_app start
+
---
+++ service.js
@@ -0,0 +1,86 @@
+var sys = require('sys');
+var fs = require('fs');
+var daemon = require('daemon');
+
+//
+// this utility allows us to easily make init.d service wrappers
+// around node scripts.
+//
+
+var args = process.argv;
+var run_args;
+
+exports.run = function(run_args_a){
+
+	run_args = run_args_a;
+
+	switch(args[2]){
+
+		case "stop":
+			stop();
+			process.exit(0);
+	                break;
+
+		case "start":
+			start();
+			break;
+
+		case "restart":
+			stop();
+			start();
+			break;
+
+		case "status":
+			var pid = getPid(run_args.lockFile);
+			if (pid){
+				sys.puts("Running, pid="+pid);
+			}else{
+				sys.puts("Not running");
+			}
+			process.exit(0);
+			break;
+
+		case "simple":
+			break;
+
+		default:
+			sys.puts('Usage: [start|stop|status|simple]');
+			process.exit(0);
+	}
+}
+
+function start(){
+	var pid = getPid(run_args.lockFile);
+	if (pid){
+		sys.puts('Already running');
+		process.exit(0);
+	}
+
+	fs.open(run_args.logFile, 'w+', function (err, fd){
+		if (err){
+			sys.puts('Error starting daemon: ' + err);
+			process.exit(1);
+		}
+		daemon.start(fd);
+		daemon.lock(run_args.lockFile);
+	});
+}
+
+function stop(){
+	var pid = getPid(run_args.lockFile);
+	if (pid){
+		process.kill(pid);
+		fs.unlinkSync(run_args.lockFile);
+		sys.puts('OK');
+	}else{
+		sys.puts('Not running');
+	}
+}
+
+function getPid(filename){
+	try {
+		return parseInt(fs.readFileSync(filename));
+	} catch (e){
+		return 0;
+	}
+}
